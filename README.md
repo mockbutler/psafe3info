3a091c4624237bb73d694add43134eea4e8ee953
Marc Butler <mockbutler@gmail.com>
2019-11-18 08:57:08 +1100

    Update to Ocaml 4.06 and Dune
    Found I could not build with 4.08 nor 4.09. I'm guessing the code
    generated by the bitstring ppx driver references the deprecated Core.Std
    namespace.

--- README.md
+++ README.md
@@ -1,19 +1,11 @@
 # psafe3info
 
-Dead basic utility for dumping the file prologue of Password Safe
-version 3 files. Really just an excuse to try out the Bitstring
-module in [Ocaml].
+Demonstration of using the Bitstring module/ppx.
+Prints header files from a Password Safe V3 data file.
 
-## Build Setup
+## Requirements
 
-The [Oasis][Oasis] build tool is used. Consult the Oasis documentation for how
-to generate build files.
-
-This application depends on the following packages:
-
- - Core
+ - OCaml 4.06
  - Bitstring
+ - Dune
 
-[RWO]: https://realworldocaml.org/
-[Ocaml]: http://ocaml.org/
-[Oasis]: https://ocaml.org/learn/tutorials/setting_up_with_oasis.html
--- psafe3info.ml
+++ psafe3info.ml
@@ -1,10 +1,24 @@
-open Core.Std
-open Printf
+(*
+Dump the header of a Password Safe Version 3 file.
+
+Notes
+
+- Cannot build with Ocaml 4.08 as it gets errors about Stdlib.Pervasives. I
+  wonder if this has something to do with the ppx deriver.
+*)
+
+open Core
+
+
+let dump_field name field =
+  printf "%s\n" name;
+  Bitstring.hexdump_bitstring stdout field
+
 
 let psafe3_dump file () =
   let bits = Bitstring.bitstring_of_file file in
-  bitmatch bits with
-  | { "PWS3" : 32 : string;
+  match%bitstring bits with
+  | {| "PWS3" : 4 * 8 : string;
       salt : 256 : bitstring;
       iter : 32 : littleendian;
       stretched_key : 256 : bitstring;
@@ -15,34 +29,30 @@ let psafe3_dump file () =
       b4 : 128 : bitstring;
       iv : 128 : bitstring;
       _ : -1 : bitstring
-    } ->
+    |} ->
     printf "PSAFE3 FILE\n";
-    printf "ITER = %ld\n" iter;
-    printf "SALT\n";
-    Bitstring.hexdump_bitstring stdout salt;
-    printf "P'\n";
-    Bitstring.hexdump_bitstring stdout stretched_key;
-    printf "H(P')\n";
-    Bitstring.hexdump_bitstring stdout stretched_key_hash;
-    printf "B1\n";
-    Bitstring.hexdump_bitstring stdout b1;
-    printf "B2\n";
-    Bitstring.hexdump_bitstring stdout b2;
-    printf "B3\n";
-    Bitstring.hexdump_bitstring stdout b3;
-    printf "B4\n";
-    Bitstring.hexdump_bitstring stdout b4;
-    printf "IV\n";
-    Bitstring.hexdump_bitstring stdout iv
-  | { _ } ->
+    printf "Iterations = %ld\n" iter;
+    dump_field "Salt" salt;
+    dump_field "P prime" stretched_key;
+    dump_field "Hash P prime" stretched_key_hash;
+    dump_field "B1" b1;
+    dump_field "B2" b2;
+    dump_field "B3" b3;
+    dump_field "B4" b4;
+    dump_field "IV" iv;
+  | {| _ |} ->
     failwith "Not a psafe3 file."
+
+
+let filename_param = Command.Param.(anon ("filename" %: string))
+
 
 let command =
   Command.basic
     ~summary:"Dump psafe3 file prologue."
     ~readme:(fun () -> "Does not decrypt the file.")
-    Command.Spec.(empty +> anon ("filename" %: string))
-    psafe3_dump
+    (Command.Param.map filename_param  ~f:(fun filename -> psafe3_dump filename))
+
 
 let () =
   Command.run ~version:"0.1" ~build_info:"mockbutler@gmail.com" command
